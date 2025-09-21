a6fb336632c02902b8de6754424887e8a20c40b4
P1nkSp0ng3 <development@p1nksp0ng3.xyz>
2025-09-21 18:17:01 +0100

    chore: installed nodemon in project files

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-/*
-    this file contains the JavaScript logic to spin up the API's hosting express server
-*/
+// imports
 const http = require('http'); // import http package
 const app = require('./app.js');
-const port = process.env.PORT || 7890;  // local port number
+const port = process.env.PORT || 7890; // local port number
 
 const server = http.createServer(app); // app is passed as a listener (the file gets executed every time a request is received)
 
