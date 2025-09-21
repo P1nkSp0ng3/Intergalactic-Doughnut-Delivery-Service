5b62e57a1b72b57e92ff3ee496161665ecc78da6
P1nkSp0ng3 <development@p1nksp0ng3.xyz>
2025-09-21 18:53:22 +0100

    feat: implemented error handling middlewares

--- app.js
+++ app.js
@@ -14,5 +14,20 @@ app.use('/health', healthRoutes);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// error handling middleware(s)
+app.use((req, res, next) => { // middleware to handle calls to non-existent API endpoints
+    const error = new Error('Not found! It appears you are lost... The doughnuts are that way...');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => { // middleware to handle all other errors
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 // export the app object so it can be used in other files
 module.exports = app;
