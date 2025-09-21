d99dbad310b3cabdf4ffc6c156faf2e41bb2e0f0
P1nkSp0ng3 <crichardson242@outlook.com>
2025-09-21 18:23:14 +0100

    feat: implemented logging (morgan) middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // imports
 const express = require('express');
 const app = express(); // execute express as a function
+const morgan = require('morgan');
 const healthRoutes = require('./api/routes/health');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+// logging middleware
+app.use(morgan('dev'));
 
 // endpoint middleware(s)
 app.use('/health', healthRoutes);
